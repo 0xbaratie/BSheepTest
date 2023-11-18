8593f589133d8c72c01e36fc89aa4acb488c989e
yamapyblack <s25050730r@gmail.com>
2023-11-18 16:47:11 +0300

    add point and stamina to contract

---
+++ contracts/contracts/SheepUp1155.sol
@@ -0,0 +1,155 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import {ERC1155} from "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+
+import "hardhat/console.sol";
+
+contract SheepUp1155 is ERC1155 {
+    struct SheepStatus {
+        uint level;
+        uint shippedAt;
+    }
+    struct PlayerTapCount {
+        uint count;
+        uint tappedAt;
+    }
+    struct PlayerShipCount {
+        uint count;
+        uint shippedAt;
+    }
+
+    event Sheepened(uint indexed sheepId, uint level, uint shippedAt);
+    event Shipped(address indexed sender, uint level);
+
+    /**
+     * id 1: sheep count
+     * id 2: tap count
+     * id 3: ship count
+     */
+    uint constant SHEEP_ID = 1;
+    uint constant TAP_ID = 2;
+    uint constant SHIP_ID = 3;
+    //caps
+    uint public constant SHIP_CAP = 10000000;
+    uint public constant PLAYER_TAP_COUNT_CAP = 100;
+    uint public constant PLAYER_SHIP_COUNT_CAP = 3;
+    //staminas
+    uint public constant TAP_COUNT_RECOVER_SECOND = 108;
+    uint public constant SHIP_COUNT_RECOVER_SECOND = 3600;
+
+    /** variables */
+    uint public maxSheepCount = 0;
+    uint public maxShipCount = 0;
+    // mapping( sheepId => SheepStatus )
+    mapping(uint => SheepStatus) public sheepStatus;
+    // mapping( player => stamina )
+    mapping(address => PlayerTapCount) public playerTapCount;
+    mapping(address => PlayerShipCount) public playerShipCount;
+    // mapping( player => point )
+    mapping(address => uint) public point;
+
+    constructor() ERC1155("") {}
+
+    function uri(uint256 _id) public view override returns (string memory) {
+        //TODO
+    }
+
+    function getPlayerTapStamina(address _sender) public view returns (uint) {
+        uint _count = playerTapCount[_sender].count;
+        if (_count > 0) {
+            uint _tappedAt = playerTapCount[_sender].tappedAt;
+            uint _reducedCount = (block.timestamp - _tappedAt) /
+                TAP_COUNT_RECOVER_SECOND;
+
+            _count = (_count < _reducedCount) ? 0 : _count - _reducedCount;
+        }
+        return PLAYER_TAP_COUNT_CAP - _count;
+    }
+
+    function getPlayerShipStamina(address _sender) public view returns (uint) {
+        uint _count = playerShipCount[_sender].count;
+        if (_count > 0) {
+            uint _shippedAt = playerShipCount[_sender].shippedAt;
+            uint _reducedCount = (block.timestamp - _shippedAt) /
+                SHIP_COUNT_RECOVER_SECOND;
+
+            _count = (_count < _reducedCount) ? 0 : _count - _reducedCount;
+        }
+        return PLAYER_SHIP_COUNT_CAP - _count;
+    }
+
+    function mint() external {
+        maxSheepCount++;
+        _mint(msg.sender, SHEEP_ID, 1, "");
+    }
+
+    function tap(uint _sheepId) external {
+        uint _count = playerTapCount[msg.sender].count;
+        if (_count > 0) {
+            uint _tappedAt = playerTapCount[msg.sender].tappedAt;
+            uint _reducedCount = (block.timestamp - _tappedAt) /
+                TAP_COUNT_RECOVER_SECOND;
+
+            _count = (_count < _reducedCount) ? 0 : _count - _reducedCount;
+        }
+        if (_count >= PLAYER_TAP_COUNT_CAP) {
+            revert("Tap cap reached");
+        }
+        playerTapCount[msg.sender] = PlayerTapCount(
+            _count + 1,
+            block.timestamp
+        );
+
+        if (_sheepId > maxSheepCount) {
+            revert("Sheep does not exist");
+        }
+
+        point[msg.sender]++;
+
+        SheepStatus storage _status = sheepStatus[_sheepId];
+        _status.level++;
+        emit Sheepened(_sheepId, _status.level, _status.shippedAt);
+
+        _mint(msg.sender, TAP_ID, 1, "");
+    }
+
+    function ship(uint _sheepId) external {
+        uint _count = playerShipCount[msg.sender].count;
+        if (_count > 0) {
+            uint _shippedAt = playerShipCount[msg.sender].shippedAt;
+            uint _reducedCount = (block.timestamp - _shippedAt) /
+                SHIP_COUNT_RECOVER_SECOND;
+
+            _count = (_count < _reducedCount) ? 0 : _count - _reducedCount;
+        }
+        if (_count >= PLAYER_SHIP_COUNT_CAP) {
+            revert("Ship cap reached");
+        }
+        playerShipCount[msg.sender] = PlayerShipCount(
+            _count + 1,
+            block.timestamp
+        );
+
+        if (maxShipCount >= SHIP_CAP) {
+            revert("Ship cap reached");
+        }
+
+        if (_sheepId > maxSheepCount) {
+            revert("Sheep does not exist");
+        }
+
+        //emit the level of sheep
+        uint _oldLevel = sheepStatus[_sheepId].level;
+        point[msg.sender] += (_oldLevel * 10);
+        emit Shipped(msg.sender, _oldLevel);
+
+        //initiate the sheep
+        uint __shippedAt = block.timestamp;
+        sheepStatus[_sheepId] = SheepStatus(0, __shippedAt);
+        emit Sheepened(_sheepId, 0, __shippedAt);
+
+        _mint(msg.sender, SHIP_ID, 1, "");
+        maxShipCount++;
+    }
+}
---
+++ contracts/test/SheepUp1155.ts
@@ -0,0 +1,217 @@
+import {
+  loadFixture,
+  time,
+} from "@nomicfoundation/hardhat-toolbox-viem/network-helpers";
+import { expect } from "chai";
+import hre from "hardhat";
+import { getAddress, parseGwei } from "viem";
+
+describe("SheepUp1155", function () {
+  const SHEEP_COUNT = 1n;
+  const TAP_COUNT = 2n;
+  const SHIP_COUNT = 3n;
+
+  async function deployFixture() {
+    const [owner, alice] = await hre.viem.getWalletClients();
+
+    const sheepUp1155 = await hre.viem.deployContract("SheepUp1155", []);
+
+    const publicClient = await hre.viem.getPublicClient();
+
+    return {
+      sheepUp1155,
+      owner,
+      alice,
+      publicClient,
+    };
+  }
+
+  describe("Tests", function () {
+    it("Mint", async function () {
+      const { sheepUp1155, owner, alice } = await loadFixture(deployFixture);
+
+      await sheepUp1155.write.mint();
+      await sheepUp1155.write.mint();
+      await sheepUp1155.write.mint({ account: alice.account.address });
+
+      //balance
+      expect(
+        await sheepUp1155.read.balanceOf([owner.account.address, SHEEP_COUNT]),
+      ).equals(2n);
+      expect(
+        await sheepUp1155.read.balanceOf([alice.account.address, SHEEP_COUNT]),
+      ).equals(1n);
+
+      //maxcount
+      expect(await sheepUp1155.read.maxSheepCount()).equals(3n);
+    });
+
+    it("Tap", async function () {
+      const { sheepUp1155, owner, alice } = await loadFixture(deployFixture);
+
+      await sheepUp1155.write.mint();
+      await sheepUp1155.write.mint();
+      await sheepUp1155.write.mint();
+
+      //id: 1
+      expect((await sheepUp1155.read.sheepStatus([1n]))[0]).equals(0n);
+      expect((await sheepUp1155.read.sheepStatus([1n]))[1]).equals(0n);
+      await sheepUp1155.write.tap([1n]);
+      await sheepUp1155.write.tap([1n]);
+      await sheepUp1155.write.tap([1n]);
+      expect((await sheepUp1155.read.sheepStatus([1n]))[0]).equals(3n);
+      expect((await sheepUp1155.read.sheepStatus([1n]))[1]).equals(0n);
+      //expect mint ERC1155
+      expect(
+        await sheepUp1155.read.balanceOf([owner.account.address, TAP_COUNT]),
+      ).equals(3n);
+
+      //id: 3 by alice
+      await sheepUp1155.write.tap([3n], { account: alice.account.address });
+      await sheepUp1155.write.tap([3n], { account: alice.account.address });
+      expect((await sheepUp1155.read.sheepStatus([3n]))[0]).equals(2n);
+      //expect mint ERC1155
+      expect(
+        await sheepUp1155.read.balanceOf([alice.account.address, TAP_COUNT]),
+      ).equals(2n);
+    });
+
+    it("Ship", async function () {
+      const { sheepUp1155, owner } = await loadFixture(deployFixture);
+
+      await sheepUp1155.write.mint();
+      await sheepUp1155.write.mint();
+      await sheepUp1155.write.mint();
+
+      await sheepUp1155.write.tap([1n]);
+      await sheepUp1155.write.tap([1n]);
+      await sheepUp1155.write.tap([1n]);
+      await sheepUp1155.write.tap([1n]);
+      await sheepUp1155.write.tap([1n]);
+      expect((await sheepUp1155.read.sheepStatus([1n]))[0]).equals(5n);
+
+      const currentTimestamp = new Date().getTime() / 1000;
+      await sheepUp1155.write.ship([1n]);
+      expect((await sheepUp1155.read.sheepStatus([1n]))[0]).equals(0n);
+      expect(
+        Number((await sheepUp1155.read.sheepStatus([1n]))[1]),
+      ).approximately(currentTimestamp, 20); //timestamp
+      //expect mint ERC1155
+      expect(
+        await sheepUp1155.read.balanceOf([owner.account.address, SHIP_COUNT]),
+      ).equals(1n);
+    });
+
+    it("Sheepened Event", async function () {
+      const { sheepUp1155, publicClient } = await loadFixture(deployFixture);
+
+      await sheepUp1155.write.mint();
+
+      const hash = await sheepUp1155.write.tap([1n]);
+      await publicClient.waitForTransactionReceipt({ hash });
+
+      const sheepenedEvents = await sheepUp1155.getEvents.Sheepened();
+      expect(sheepenedEvents[0].args.sheepId).equals(1n);
+      expect(sheepenedEvents[0].args.level).equals(1n);
+      expect(sheepenedEvents[0].args.shippedAt).equals(0n);
+    });
+
+    it("Shipped Event", async function () {
+      const { sheepUp1155, owner, publicClient } =
+        await loadFixture(deployFixture);
+
+      await sheepUp1155.write.mint();
+      await sheepUp1155.write.tap([1n]);
+      await sheepUp1155.write.tap([1n]);
+
+      const hash = await sheepUp1155.write.ship([1n]);
+      await publicClient.waitForTransactionReceipt({ hash });
+
+      const shippedEvents = await sheepUp1155.getEvents.Shipped();
+      expect(
+        shippedEvents[0].args.sender?.toString().toLocaleLowerCase(), //lower case
+      ).equals(owner.account.address);
+      expect(shippedEvents[0].args.level).equals(2n);
+    });
+
+    it("Stamina", async function () {
+      const { sheepUp1155, owner } = await loadFixture(deployFixture);
+
+      await sheepUp1155.write.mint();
+
+      expect(
+        await sheepUp1155.read.getPlayerTapStamina([owner.account.address]),
+      ).equals(100n);
+      expect(
+        await sheepUp1155.read.getPlayerShipStamina([owner.account.address]),
+      ).equals(3n);
+
+      await sheepUp1155.write.tap([1n]);
+      await sheepUp1155.write.tap([1n]);
+      await sheepUp1155.write.tap([1n]);
+      expect(
+        await sheepUp1155.read.getPlayerTapStamina([owner.account.address]),
+      ).equals(97n);
+
+      //reduce to 0
+      for (let i = 0; i < 97; i++) {
+        await sheepUp1155.write.tap([1n]);
+      }
+      expect(
+        await sheepUp1155.read.getPlayerTapStamina([owner.account.address]),
+      ).equals(0n);
+      await expect(sheepUp1155.write.tap([1n])).to.be.rejectedWith(
+        "Tap cap reached",
+      );
+
+      //recover
+      await time.increase(108);
+      expect(
+        await sheepUp1155.read.getPlayerTapStamina([owner.account.address]),
+      ).equals(1n);
+
+      //ship
+      await time.increase(108);
+      expect(
+        await sheepUp1155.read.getPlayerTapStamina([owner.account.address]),
+      ).equals(2n);
+
+      await sheepUp1155.write.mint();
+      await sheepUp1155.write.mint();
+      await sheepUp1155.write.tap([2n]);
+      await sheepUp1155.write.tap([3n]);
+      await sheepUp1155.write.ship([1n]);
+      await sheepUp1155.write.ship([2n]);
+      await sheepUp1155.write.ship([3n]);
+      expect(
+        await sheepUp1155.read.getPlayerShipStamina([owner.account.address]),
+      ).equals(0n);
+
+      //recover
+      await time.increase(3600);
+      expect(
+        await sheepUp1155.read.getPlayerShipStamina([owner.account.address]),
+      ).equals(1n);
+      await time.increase(360000);
+      expect(
+        await sheepUp1155.read.getPlayerShipStamina([owner.account.address]),
+      ).equals(3n);
+    });
+
+    it("Point", async function () {
+      const { sheepUp1155, owner, alice } = await loadFixture(deployFixture);
+      await sheepUp1155.write.mint();
+      for (let i = 0; i < 20; i++) {
+        await sheepUp1155.write.tap([1n]);
+      }
+      for (let i = 0; i < 10; i++) {
+        await sheepUp1155.write.tap([1n], { account: alice.account.address });
+      }
+      await sheepUp1155.write.ship([1n], { account: alice.account.address });
+      expect(await sheepUp1155.read.point([owner.account.address])).equals(20n);
+      expect(await sheepUp1155.read.point([alice.account.address])).equals(
+        310n,
+      );
+    });
+  });
+});
