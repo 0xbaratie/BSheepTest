fdb6fe034de637c913a6a6ed2e39d5faf42f59ea
libdefi <libcreatesweb3@gmail.com>
2023-11-18 19:10:54 +0300

    Color fixed

--- sheepUi/pages/index.tsx
+++ sheepUi/pages/index.tsx
@@ -72,9 +72,9 @@ const Index = () => {
     return (
       <div className="hidden sm:flex flex-col items-center justify-center bg-blue h-screen">
         <img src="/images/sheep.svg" alt="Sheep Icon" className="w-18 h-18" />
-        <p className="p-2 font-bold text-4xl">Sheep It</p>
-        <p className="p-2 font-bold text-md">An onchain Sheeping (Sheep * ship it) game</p>
-        <div className='bg-gray rounded-md p-4 mt-8'>
+        <p className="p-2 font-bold text-4xl text-white">Sheep It</p>
+        <p className="p-2 font-bold text-md text-white">An onchain Sheeping (Sheep * ship it) game</p>
+        <div className='bg-gray rounded-sm p-4 mt-8'>
           <p className="text-black font-bold">
             Sheep It is only on mobile. Visit on your phone to play.
           </p>
