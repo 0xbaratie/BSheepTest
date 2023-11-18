6c0f96db0af3e262c5c50cf02cf360f13a250720
libdefi <libcreatesweb3@gmail.com>
2023-11-18 23:53:09 +0300

    Update data.ts

--- sheepUi/utils/data.ts
+++ sheepUi/utils/data.ts
@@ -1,19 +1,19 @@
 import Sheep from '../public/images/sheep.svg';
 export const sheepData = [
   {
-    id: "0",
+    id: 0,
     level: 1,
   },
   {
-    id: "1",
+    id: 1,
     level: 1,
   },
   {
-    id: "2",
+    id: 2,
     level: 1,
   },
   {
-    id: "3",
+    id: 3,
     level: 122,
   },
 ];
