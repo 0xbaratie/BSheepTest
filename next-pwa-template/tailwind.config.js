4f47e00227056686833dca070d9825a50e07fbd2
libdefi <libcreatesweb3@gmail.com>
2023-11-18 06:59:56 +0300

    Create smartphone-only constraint screens for Sheep It

--- next-pwa-template/tailwind.config.js
+++ next-pwa-template/tailwind.config.js
@@ -3,6 +3,15 @@ module.exports = {
 		'./pages/**/*.{js,ts,jsx,tsx}',
 		'./components/**/*.{js,ts,jsx,tsx}',
 	],
+	theme: {
+    extend: {
+      colors: {
+        blue: '#3866F6',
+				black: '#23262F',
+				gray: '#D9D9D9',
+      },
+    },
+  },
 	darkMode: 'class',
 	plugins: [require('tailwindcss-safe-area')],
 }
